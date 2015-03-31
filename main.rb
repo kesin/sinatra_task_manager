1dd4dfc1aee33885400cefc826bf643ed198da8c
Zoker <kaixuanguiqu@gmail.com>
2015-03-31 20:11:59 +0800

    sinatra base route and params

--- main.rb
+++ main.rb
@@ -5,19 +5,7 @@ get '/' do
   slim :index
 end
 
-__END__
-
-@@layout
-doctype html
-html
-  head
-    meta chatset='utf-8'
-    title Just Do It
-  body
-    h1 Just Do It
-    == yield
-
-@@index
-h2 My Tasks
-ul.tasks
-  li Get Milk
+get '/:task' do
+  @task = params[:task].split('-').join(' ').capitalize
+  slim :task
+end
