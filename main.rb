abc8929fe7ec0315b26c1797adbfbaa8f18783cf
Zoker <kaixuanguiqu@gmail.com>
2015-03-31 23:40:22 +0800

    add create tasks func

--- main.rb
+++ main.rb
@@ -1,13 +1,26 @@
 require 'sinatra'
+require 'data_mapper'
 require 'slim'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Task
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String, :required => true
+  property :completed_at, DateTime
+end
+
+DataMapper.finalize
+
 get '/' do
+  @tasks = Task.all
   slim :index
 end
 
 post '/' do
-  @task = params[:task]
-  slim :task
+  Task.create params[:task]
+  redirect '/'
 end
 
 get '/:task' do
